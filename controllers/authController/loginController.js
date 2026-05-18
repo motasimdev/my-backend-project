f4408a43633528e42049a3798a6a3228ab768893
MD Motasim Billah <themotasimdev@gmail.com>
2026-05-18 20:22:04 +0600

    login

--- controllers/authController/loginController.js
+++ controllers/authController/loginController.js
@@ -26,26 +26,36 @@ async function loginController(req, res) {
   }
   // email format regex diye validate
 
-  // password hash kora
-  const hash = await bcrypt.hash(password, 10);
-  // password hash kora
-
   const duplicateUser = await userSchema.findOne({ email });
 
+  // email diye signup hoise ki na sheta check
   if (!duplicateUser) {
     return res.json({
       error: "Email Not Found",
     });
   }
+  // email diye signup hoise ki na sheta check
 
-  //   if (hash !== duplicateUser.password) {
-  //     return res.json({
-  //       error: "Password Wrong",
-  //     });
-  //   }
+  // email diye signup krar por verify hoise ki na sheta check
+  if (!duplicateUser.isVerified) {
+    return res.json({
+      error: "User is Not Verified",
+    });
+  }
+  // email diye signup krar por verify hoise ki na sheta check
+
+  // password hash baniye erpr compare kora
+  const isMatch = await bcrypt.compare(password, duplicateUser.password);
+
+  if (!isMatch) {
+    return res.json({
+      error: "Password Wrong",
+    });
+  }
+  // password hash compare kora
 
   res.json({
-    message: "login Success"
+    message: "login Success",
   });
 }
 
